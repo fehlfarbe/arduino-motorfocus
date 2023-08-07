34f81408e15a55ffc32892a63393a6e74e8c5b6a
fehlfarbe <marcus@degenkolbe.eu>
2023-08-07 19:12:09 +0200

    Added build and CI scripts

---
+++ build_envs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+BUILD_DIR=".pio/build/"
+RELEASE_DIR="./release"
+TAG=$(git describe --tags)
+
+# make release directory
+mkdir -p ${RELEASE_DIR}
+
+# build all configs
+pio run
+
+# copy builds to release
+for config in `ls -d ${BUILD_DIR}/*/`
+do
+    config_name=$(basename "$config")
+    if [[ "${config_name}" != *"debug"* ]]; then
+        echo $config
+        # gzip -c -f "${config}/firmware.hex" > "${RELEASE_DIR}/${config_name}-${TAG}.gz"
+        cp "${config}/firmware.hex" "${RELEASE_DIR}/${config_name}-${TAG}.hex"
+    fi
+done
+
+ls -lh ${RELEASE_DIR}
